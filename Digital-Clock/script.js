d2c32d785e3fa1918826ad5b0878cd0ca34f7667
vishwamohini15 <vishwamohini0015@gmail.com>
2024-12-13 16:17:32 +0530

    digital-Clock

---
+++ Digital-Clock/script.js
@@ -0,0 +1,9 @@
+let clock=document.querySelector("#clock");
+
+
+setInterval(()=>{
+     let date=new Date();
+     // console.log(date.toLocaleTimeString());
+     clock.innerHTML=date.toLocaleTimeString();
+     
+}, 1000)
